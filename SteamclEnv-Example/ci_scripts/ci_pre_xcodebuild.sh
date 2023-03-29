c6fe853355ec5b251a872092c8aaa28026c3c71d
brendan <brendan@steamclock.com>
2023-03-29 15:24:48 -0700

    refine generator script, add some documentation

--- SteamclEnv-Example/ci_scripts/ci_pre_xcodebuild.sh
+++ SteamclEnv-Example/ci_scripts/ci_pre_xcodebuild.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 echo "Writing .env file to root directory"
 
-echo "API_URL=1$API_URL" >| "../.env"
+echo "API_URL=$API_URL" >| "./.env"
 
-echo "Wrote .env file."
+echo "Wrote .env file. Running steamclenv generate..."
 
-swift run steamclenv generate --path "../.env" --output-path "../"
+# We're in the ./ci_scripts folder here, so need to step up one level
+swift run steamclenv generate --output-path "../"
 
 exit 0
